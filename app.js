422eb32ed5068623c83ec5bb6bb51a61d855e2c9
fast-tortoise <1rn18is121@gmail.com>
2023-01-18 18:10:10 +0530

    Dynamic list creation and modification added

--- app.js
+++ app.js
@@ -16,8 +16,13 @@ const listSchema = new mongoose.Schema({
   listItem : String
 })
 
+const List = new mongoose.Schema({
+  listName : String,
+  listItems : [listSchema]
+})
+
+const ListModel = mongoose.model('List', List)
 const NormalItem = mongoose.model('Item', listSchema)
-const WorkItem = mongoose.model('WorkItem', listSchema)
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -25,15 +30,35 @@ app.use(express.static("public"));
 
 var completedTask = "";
 
-async function getWorkList(){
-  const workList = await WorkItem.find({});
-}
-getWorkList();
-
-// app.get('/:newTodo', (req, res) => {
-//   variable = mongoose.model(variable, listSchema);
-//   res.render("list", {listTitle: req.params.newTodo, newListItems: normalList});
-// })
+app.get('/:newTodo', (req, res) => {
+  const newTodo = req.params.newTodo;
+  var newList;
+  async function createNewList(){
+  await ListModel.findOne({listName: newTodo}, function (err, docs) {
+      if (!err){
+        if (!docs){
+          console.log(err)
+          newList = new ListModel({
+            listName : newTodo,
+            listItems : []
+          });
+          newList.save();
+          res.redirect('/'+ newTodo);
+        }
+        else{
+          newList = docs;
+          var listToSend = newList.listItems;
+          res.render("list", {listTitle: req.params.newTodo, newListItems: listToSend});
+        }
+      }
+      else{
+        console.log(err)
+      }
+    }).clone();
+  }
+  createNewList();
+  
+})
 
 app.get("/", function(req, res) {
   async function getCommonList(){
@@ -44,19 +69,33 @@ app.get("/", function(req, res) {
   
 });
 
+async function postHanderler(item, list){
+  console.log("I'm    *****************************************  here") 
+  const newItem = new NormalItem({ listItem: item });
+  ListModel.findOne({listName: list}, function (err, listDoc){
+    if(!err){
+      listDoc.listItems.push(newItem);
+      listDoc.save();
+    }
+    else{
+      console.log(err)
+    }
+  });
+}
+
 app.post("/", function(req, res){
   const item = req.body.newItem;
+  const list = req.body.list;
   console.log(req.body);
-  // if (req.body.list === "Work") {
-  //   const newWork = new WorkItem({listItem:item})
-  //   newWork.save();
-  //   res.redirect("/work");
-  // } 
-  // else {
+  if(list == 'ToDoList'){
     const newItem = new NormalItem({listItem: item});
     newItem.save();
     res.redirect("/");
-  // }
+  }
+    else{
+      postHanderler(item, list);
+      res.redirect("/"+list);
+    }
 });
 
 app.get("/about", function(req, res){
@@ -66,16 +105,6 @@ app.get("/about", function(req, res){
 app.post("/delete", function(req, res){
   completedTask = req.body.checkbox
   console.log(typeof completedTask)
-  // if (workItems.includes(completedTask)){
-  //   const newWorkList = workItems.filter(function(item){
-  //     return item !== completedTask
-  //   })
-  //   workItems = newWorkList;
-  //   deleteOne();
-  //   res.redirect("/work" )
-  // }
-  
-  // else{
     async function deleteOneMain(){
       var del= await NormalItem.deleteOne({ listItem: completedTask })
       console.log(del)
@@ -84,11 +113,6 @@ app.post("/delete", function(req, res){
     res.redirect("/")
   // }
 })
-
-async function deleteOne(){
-  var del= await WorkItem.deleteOne({ listItem: completedTask })
-  console.log(del)
-}
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
